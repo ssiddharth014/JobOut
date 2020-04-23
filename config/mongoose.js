209ac2d7431cf157df74282b7ff2ab4e3cfbee17
siddharth <ssiddharth014@gmail.com>
2020-04-23 11:06:43 +0530

    mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -33,8 +33,8 @@ console.log('connected..');
 module.exports=db;
 */
 const mongoose= require('mongoose');
-//const URL="mongodb+srv://foodshalauser:foodshala@foodshalacluster-xsd3l.mongodb.net/test?retryWrites=true&w=majority";
-const URL ="mongodb+srv://jobout:jobout@jobout-cqnps.mongodb.net/test?retryWrites=true&w=majority";
+const URL="mongodb+srv://foodshalauser:foodshala@foodshalacluster-xsd3l.mongodb.net/test?retryWrites=true&w=majority";
+//const URL ="mongodb+srv://jobout:jobout@jobout-cqnps.mongodb.net/test?retryWrites=true&w=majority";
 const db=async() =>{
 await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true})
 console.log('connected..');
